e8d1cfc4606f2cd05eae53f22f88457842011577
Raluca Parvan <parvanraluca0@gmail.com>
2023-11-22 16:16:19 +0100

    Migrate CategoriesContext to Redux

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,6 @@ import { BrowserRouter } from 'react-router-dom'
 import { Provider } from 'react-redux'
 import { store } from './store/store'
 import App from './App.jsx'
-import { CategoriesProvider } from './context/CategoriesContext'
 import { CartProvider } from './context/CartContext'
 import './main.scss'
 
@@ -12,11 +11,9 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
       <BrowserRouter>
-          <CategoriesProvider>
             <CartProvider>
               <App />
             </CartProvider>
-          </CategoriesProvider>
       </BrowserRouter>
     </Provider>
   </React.StrictMode>,
--- src/routes/shop/Shop.jsx
+++ src/routes/shop/Shop.jsx
@@ -1,8 +1,26 @@
 import { Routes, Route } from "react-router-dom";
+
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+
+import { getCollectionAndDocuments } from "../../utils/firebase";
+import { setCategoriesMap } from '../../store/categories/category.action';
+
 import CategoriesPreview from '../categories-preview/CategoriesPreview';
 import Category from "../category/Category";
 
 const Shop = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const getCategoriesMap = async () => {
+      const categoryMap = await getCollectionAndDocuments();
+      dispatch(setCategoriesMap(categoryMap));
+    }
+
+    getCategoriesMap();
+  }, [dispatch]);
+
   return (
     <Routes>
       <Route index element={<CategoriesPreview /> } />
