fc01221e9644364dbb5ac0e7dfb0f34ccb0586cb
Raluca Pârvan <parvanraluca0@gmail.com>
2024-01-11 19:11:09 +0100

    Merge branch

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,8 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import { Provider } from 'react-redux'
-import { Elements } from '@stripe/react-stripe-js'
+import { PersistGate } from 'redux-persist/lib/integration/react';
+import { Elements } from '@stripe/react-stripe-js';
 
 import { store, persistor } from './store/store'
 import { stripePromise } from './utils/stripe/stripe.utils'
@@ -13,10 +14,8 @@ import './main.scss'
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
+      <PersistGate persistor={persistor}>
         <BrowserRouter>
-          <Elements stripe={stripePromise}>
-            <App />
-          </Elements>
           <Elements stripe={stripePromise}>
             <App />
           </Elements>
