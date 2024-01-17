3c71b00180459e6c984d474827f50ca890b1d448
Raluca Pârvan <parvanraluca0@gmail.com>
2024-01-17 20:34:58 +0100

    Implement redux-saga for categories

--- src/routes/shop/Shop.jsx
+++ src/routes/shop/Shop.jsx
@@ -3,7 +3,7 @@ import { Routes, Route } from "react-router-dom";
 import { useEffect } from "react";
 import { useDispatch } from "react-redux";
 
-import { fetchCategoriesAsync } from '../../store/categories/category.action';
+import { fetchCategoriesStart } from "../../store/categories/category.action";
 
 import CategoriesPreview from '../categories-preview/CategoriesPreview';
 import Category from "../category/Category";
@@ -12,7 +12,7 @@ const Shop = () => {
   const dispatch = useDispatch();
 
   useEffect(() => {
-      dispatch(fetchCategoriesAsync());
+      dispatch(fetchCategoriesStart());
   }, [dispatch]);
 
   return (
--- src/store/categories/category.action.js
+++ src/store/categories/category.action.js
@@ -1,8 +1,6 @@
 import { createAction } from "../../utils/reducer";
 import CATEGORIES_ACTION_TYPES from "./category.types";
 
-import { getCollectionAndDocuments } from "../../utils/firebase";
-
 export const fetchCategoriesStart = () => 
   createAction(CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_START);
 
@@ -13,13 +11,3 @@ export const fetchCategoriesSuccess = (categoriesArray) =>
 
 export const fetchCategoriesFailed = (error) => 
   createAction(CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_START, error);
-
-export const fetchCategoriesAsync = () => async (dispatch) => {
-  dispatch(fetchCategoriesStart());
-  try{
-    const categoriesArray = await getCollectionAndDocuments();
-    dispatch(fetchCategoriesSuccess(categoriesArray));
-  } catch (error) {
-    dispatch(fetchCategoriesFailed(error));
-  }
-}
